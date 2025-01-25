d8ad2e517ad475ca39bf455035211a2207942728
Prayas Godara <76145007+prayas59@users.noreply.github.com>
2025-01-25 07:55:17 +0000

    datatypes

---
+++ 01_basics/00_consoleLog.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.log("Prayas")
+console.error("risk")
+console.warn("risk")
+console.table([accountId, accountEmail, accountPassword, accountCity])
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Js
 A code repo for learning JS
+
+https://tc39.es/ecma262/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
